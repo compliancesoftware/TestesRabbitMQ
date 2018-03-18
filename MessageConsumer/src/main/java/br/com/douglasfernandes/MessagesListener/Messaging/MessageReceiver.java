281195f47e48da6fcd1dd99d9bc93519c02175bd
Douglas Fernandes <douglasf.filho@gmail.com>
2018-03-18 14:39:46 -0300

    Mudança na estrutura do projeto
    Signed-off-by: Douglas Fernandes <douglasf.filho@gmail.com>

--- MessageConsumer/src/main/java/br/com/douglasfernandes/MessagesListener/Messaging/MessageReceiver.java
+++ MessageConsumer/src/main/java/br/com/douglasfernandes/MessagesListener/Messaging/MessageReceiver.java
@@ -1,17 +1,9 @@
 package br.com.douglasfernandes.MessagesListener.Messaging;
 
-import br.com.douglasfernandes.MessagesListener.models.ResponseBody;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import sun.security.provider.certpath.OCSPResponse;
-
-import java.io.IOException;
-import java.io.Serializable;
 
 @Component
 @Log4j
--- MessageSender/src/main/java/br/com/douglasfernandes/UserServices/Messaging/MessageSender.java
+++ MessageSender/src/main/java/br/com/douglasfernandes/UserServices/Messaging/MessageSender.java
@@ -1,24 +1,15 @@
 package br.com.douglasfernandes.UserServices.Messaging;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-
-import java.io.Serializable;
 
 @Service
 @Log4j
 public class MessageSender {
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @Autowired
     @Qualifier("rabbitTemplate")
